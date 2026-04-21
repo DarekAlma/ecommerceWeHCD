d517047538eaa1cc859916f7b24671d2e8b914ef
MariSalas23 <mari.masagu@gmail.com>
2026-04-20 21:08:16 -0500

    Rutas protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,132 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import { useEffect, useState } from "react";
 
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-import Header from "./components/header/Header";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Cart from "./pages/Cart/Cart";
 import Perfil from "./pages/perfil/Perfil";
 import Start from "./pages/start/Start";
 
+import { escucharSesion } from "./firebase/auth";
+
+function RutaProtegida({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<any>(undefined);
+
+  useEffect(() => {
+    const unsubscribe = escucharSesion((usuario) => {
+      setUser(usuario);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (user === undefined) return <div>Cargando...</div>;
+
+  return user ? children : <Navigate to="/login" replace />;
+}
+
+function RutaPublica({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<any>(undefined);
+
+  useEffect(() => {
+    const unsubscribe = escucharSesion((usuario) => {
+      setUser(usuario);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (user === undefined) return <div>Cargando...</div>;
+
+  return user ? <Navigate to="/start" replace /> : children;
+}
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/start" element={<Start />} />
+
+        {/* Públicas */}
+        <Route
+          path="/"
+          element={
+            <RutaPublica>
+              <Login />
+            </RutaPublica>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <RutaPublica>
+              <Login />
+            </RutaPublica>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <RutaPublica>
+              <Signup />
+            </RutaPublica>
+          }
+        />
+
+        {/* Privadas */}
+        <Route
+          path="/start"
+          element={
+            <RutaProtegida>
+              <Start />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/home"
+          element={
+            <RutaProtegida>
+              <Home />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/product"
+          element={
+            <RutaProtegida>
+              <Product />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/cart"
+          element={
+            <RutaProtegida>
+              <Cart />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/perfil"
+          element={
+            <RutaProtegida>
+              <Perfil />
+            </RutaProtegida>
+          }
+        />
 
       </Routes>
     </Router>
